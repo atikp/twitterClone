b8d88990e99e00445d5c6d0a7d1d19867498d529
atikP <atikpatel@hotmail.co.uk>
2021-04-20 17:33:38 +0100

    userHeader info added

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,25 @@
-import API from './API.js'
+import API from './API.js';
 // Your code here
+API.getTweets();
+let sessionUser = JSON.parse(sessionStorage.getItem('currentUser'));
+let userDetails = sessionUser.name;
 
-API.getTweets()
+const profilePicEl = document.querySelector('.profilePic img');
+const userNameEl = document.querySelector('.userName');
+const userTag = document.querySelector('.nameLocation .userTag');
+const locationEl = document.querySelector('.nameLocation .location');
+const newUserTag = (sessionUser.name.replace(/\s/g, '').toLowerCase());
+const followersEl = document.querySelector('.followers span');
+const followingEl = document.querySelector('.following span');
+
+profilePicEl.src = sessionUser.avatar_url;
+userNameEl.innerText = sessionUser.name;
+userTag.innerText = `@${newUserTag}`;
+followersEl.innerText = sessionUser.followers;
+followingEl.innerText = sessionUser.following;
+locationEl.innerText = sessionUser.location;
+
+console.log('sessionUser:',sessionUser);
+console.log(userDetails);
+// sessionUser.tweets.push('12334');
+// sessionUser.name = 'bob';
---
+++ app/js/login.js
@@ -0,0 +1,19 @@
+import API from './API.js'
+const users = API.getUsers();
+const loginSubmit = document.querySelector('.loginForm');
+const userName = document.querySelector('.loginForm [placeholder=Username]')
+// let userInput = 'Elbert Beer';
+
+loginSubmit.addEventListener('submit',(event)=>{
+  sessionStorage.clear();
+  event.preventDefault();
+  users.then(userList=> userList.forEach(user=>{
+    if(user.name == userName.value){
+      console.log(user.name)
+      console.log(userName);
+      window.location.href = '../feedView.html'
+      const currentUser = JSON.stringify(user);
+      sessionStorage.setItem('currentUser', `${currentUser}`);
+    }
+  }));
+})
